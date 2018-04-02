fb3075fb9b121b78e61303d6bd6b93f3df8f84d5
FredRohrer <31697432+FredRohrer@users.noreply.github.com>
2018-04-02 13:52:52 -0400

    Added some tables
    Added the create tables statements for MemberLevel, Customer, and Purchase

--- shema.sql
+++ shema.sql
@@ -1,1 +1,39 @@
 --schema.sql
+
+
+drop table MemberLevel cascade constraints;
+
+create table MemberLevel (
+	MemberLevel_ID int,
+	Name varchar2(20),
+	Booster_Factor float,
+	constraint MemberLever_pk primary key (MemberLevel_ID)
+);
+
+drop table Customer cascade constraints;
+
+create table Customer (
+	Customer_ID int, 
+	First_Name varchar2(20),
+	Last_Name varchar2(20),
+	Email varchar2(20),
+	MemberLevel_ID int,
+	Total_Points float,
+	constraint Customer_pk primary key (Customer_ID),
+	constraint Customer_fk_MemberLevel foreign key (MemberLevel_ID) 
+		references MemberLevel(MemberLevel_ID)
+); --I need to add a trigger
+
+drop table Purchase cascade constraints;
+
+create table Purchase(
+	Purchase_ID int,
+	Customer_ID int,
+	Store_ID int,
+	Purchase_Time,
+	constraint Purchase_pk primary key (Purchase_ID),
+	constraint Purchase_fk_Customer foreign key (Customer_ID)
+		references Customer(Customer_ID),
+	constraint Purchase_fk_Store foreign key (Store_ID)
+		references Store(Store_ID)
+);
