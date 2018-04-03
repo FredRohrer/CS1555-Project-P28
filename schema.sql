c010eb9fbf4dccec54ef376d6d23c87f06a80ae6
FredRohrer <fjr7@pitt.edu>
2018-04-02 22:45:42 -0400

    Added a bunch of stuff
    Fixed up the trigger and added tables to make the trigger work

---
+++ schema.sql
@@ -0,0 +1,97 @@
+--schema.sql
+
+--drop tables if they exist up here
+drop table Coffee cascade constraints;
+drop table Promotion cascade constraints;
+drop table MemberLevel cascade constraints;
+drop table Customer cascade constraints;
+drop table Purchase cascade constraints;
+
+drop table HasPromotion cascade constraints;
+drop table PromoteFor cascade constraints;
+drop table BuyCoffee cascade constraints;
+
+create table Coffee (
+	Coffee_ID int,
+	Name varchar2(20),
+	Description varchar2(20),
+	Intensity int,
+	Price float,
+	Reward_Points float,
+	Redeem_Points float,
+	constraint Coffee_pk primary key (Coffee_ID)
+);
+
+create table Promotion (
+	Promotion_ID int,
+	Name varchar2(20),
+	Start_Date date,
+	End_Date date,
+	constraint Promotion_pk primary key (Promotion_ID),
+	constraint Promotion_Time_Period check (End_Date > Start_Date)
+);
+
+create table MemberLevel (
+	MemberLevel_ID int,
+	Name varchar2(20),
+	Booster_Factor float,
+	constraint MemberLever_pk primary key (MemberLevel_ID)
+);
+
+create table Customer (
+	Customer_ID int, 
+	First_Name varchar2(20),
+	Last_Name varchar2(20),
+	Email varchar2(20),
+	MemberLevel_ID int,
+	Total_Points float,
+	constraint Customer_pk primary key (Customer_ID),
+	constraint Customer_fk_MemberLevel foreign key (MemberLevel_ID) 
+		references MemberLevel(MemberLevel_ID),
+	constraint Customer_Points_Not_Neg check (Total_Points >= 0)
+); 
+
+create table Purchase (
+	Purchase_ID int,
+	Customer_ID int,
+	Store_ID int,
+	Purchase_Time date,
+	constraint Purchase_pk primary key (Purchase_ID),
+	constraint Purchase_fk_Customer foreign key (Customer_ID)
+		references Customer(Customer_ID)--,
+	--constraint Purchase_fk_Store foreign key (Store_ID)
+	--	references Store(Store_ID)
+);
+
+create table HasPromotion (
+	Store_ID int,
+	Promotion_ID int,
+	constraint HasPromotion_pk primary key (Store_ID, Promotion_ID),
+	--constraint HasPromotion_Store_fk foreign key (Store_ID)
+	--	references Store(Store_ID),
+	constraint HasPromotion_Promo_fk foreign key (Promotion_ID)
+		references Promotion(Promotion_ID)
+);
+
+create table PromoteFor (
+	Promotion_ID int,
+	Coffee_ID int, 
+	constraint PromoteFor_pk primary key (Promotion_ID, Coffee_ID),
+	constraint PromoteFor_Promo_fk foreign key (Promotion_ID)
+		references Promotion(Promotion_ID)--,
+	--constraint PromoteFor_Cof_fk foreign key (Coffee_ID)
+	--	references Coffee(Coffee_ID)
+);
+
+create table BuyCoffee (
+	Purchase_ID int,
+	Coffee_ID int,
+	Purchase_Quantity int,
+	Redeem_Quantity int,
+	constraint BuyCoffee_pk primary key (Purchase_ID, Coffee_ID),
+	constraint BuyCoffee_Purch_fk foreign key (Purchase_ID)
+		references Purchase(Purchase_ID)--,
+	--constraint BuyCoffee_Cof_fk foreign key (Coffee_ID)
+	--	references Coffee(Coffee_ID)
+);
+
--- trigger.sql
+++ trigger.sql
@@ -1,52 +1,81 @@
 --trigger.sql
 
 create or replace function Get_Coffee_Promoted
-(coffeeID in Coffee.Coffee_ID%type, storeID in Store.Store_ID%type,
-	cur_date in Purchase.Purchase_Time%type)
+(coffeeID in int, storeID in int, cur_date in date)
 	return boolean
-	declare 
-		cursor rst = select *
-					from Promotion natural join HasPromotion natural join PromoteFor
-					where Store_ID = store_id and Coffee_ID = coffeeID and 
-					(cur_date between Start_Date and End_Date)
+	is	
+		cursor rst is (select *
+						from Promotion natural join HasPromotion natural join PromoteFor
+						where Store_ID = store_id and Coffee_ID = coffeeID and 
+						(cur_date between Start_Date and End_Date));
 	begin
+		
 		open rst;
-		return rst%found
-	end
+		return rst%found;
+	end;
 /
 
 
 create or replace function Get_Coffee_Reward
-(coffeeID in Coffee.Coffee_ID%type, 
-	storeID in Store.Store_ID%type, 
-	cur_date in Purchase.Purchase_Time%type)
-	return Customer.Total_Points%type
-	reward Customer.Total_Points%type;
+(purchID in int, coffeeID in int)
+	return float
+	is
+	reward float;
+	cusID int;
+	storeID int;
+	cur_date date;
 	begin
 		select Reward_Points into reward
 		from Coffee
 		where Coffee_ID = coffeeID;
+		--reward := 0;
+		
+		
+		select Customer_ID, Store_ID, Purchase_Time
+		into cusID, storeID, cur_date
+		from Purchase
+		where Purchase_ID = purchID;
+		
+		select Booster_Factor*reward
+		into reward
+		from Customer natural join MemberLevel
+		where Customer_ID = cusID;
 		
 		
 		if (Get_Coffee_Promoted(coffeeID, storeID, cur_date))
 		then
-			reward := reward * 2
+			reward := reward * 2;
 		end if; 
 		return reward;
-	end
+	end;
 /
-	
 
 
 create or replace trigger Update_Customer_Points
 	after insert
 	on BuyCoffee
-	for each row --Assuming a BuyCoffee will always have a purchase_Quantity or Redeem_Quantity
-	declare
-	pointsLost Customer.Total_Points%type,
-	pointsGained  Customer.Total_Points%type
+	for each row
+	declare 
+		pointsGained float;
+		pointsLost float;
 	begin
-		points_gained := :new.Purchase_Quantity * Get_Coffee_Reward(:new.Coffee_ID, 
+		pointsGained := :new.Purchase_Quantity * 
+			Get_Coffee_Reward(:new.Purchase_ID, :new.Coffee_ID);
+		
+		select Redeem_Points*:new.Redeem_Quantity
+		into pointsLost
+		from Coffee
+		where Coffee_ID = :new.Coffee_ID;
+		--pointsLost := 1;
+		
+		update Customer
+		set Total_Points = Total_Points + (pointsGained - pointsLost)
+		where Customer_ID = (select Customer_ID
+							from Purchase
+							where Purchase_ID = :new.Purchase_ID);
+		
 	end;
 /
+
+
 	
