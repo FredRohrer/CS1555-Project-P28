566b14cbf11c3c7487d26a61d3582e1ee71ad7e8
FredRohrer <31697432+FredRohrer@users.noreply.github.com>
2018-04-12 13:53:54 -0400

    Added some boiler plate
    Added an init that establishes a connect, also added a main for testing

--- BoutiqueCoffee.java
+++ BoutiqueCoffee.java
@@ -1,9 +1,42 @@
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.sql.*;
+
 
 public class BoutiqueCoffee {
-
+	
+  private Connection connection;
+  private Statement statement;
+  private ResultSet resultSet;
+  
+  //Put your username and password for your particular database here
+  private String username = "";
+  private String password = "";
+  
+  
+  //Init, I think this is how it should work, but idk.
+  //pretty sure its fine this way
+  public BoutiqueCoffee() {
+	try {
+		//Register oracle driver, whatever that means
+		DriverManager.registerDriver (new oracle.jdbc.driver.OracleDriver());
+		
+		//location of database, this is the line in the demo code but it seems wrong
+		String url = "jdbc:oracle:thin:@class3.cs.pitt.edu:1521:dbclass";
+		
+		//create the actual connection
+		connection = DriverManager.getConnection(url, username, password);
+	}
+	catch (Exception Ex) {
+	  System.out.println("Error connecting to database.  Machine Error: " + Ex.toString());
+      Ex.printStackTrace();
+	}
+	
+	//problem: I don't know when to close the connection
+  }
+  
+  
   public int addStore(String name, String address, String storeType, double gpsLong, double gpsLat) {
     return -1;
   }
@@ -59,4 +92,10 @@ public class BoutiqueCoffee {
   public List<Integer> getTopKCustomersInPastXMonth(int k, int x) {
     return new ArrayList<Integer>();
   }
+  
+  //Use this for tests
+  public static void main (String [] args) {
+	  BoutiqueCoffee test = new BoutiqueCoffee();
+  }
+  
 }
